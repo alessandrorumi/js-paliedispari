1a4115a92e9a7bace9c01412fe2daf82f463fe55
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-27 15:05:08 +0100

    Add:  Scelta risultato e numero User

---
+++ Pari e Dispari/script/myScript.js
@@ -0,0 +1,11 @@
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri 
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+// Scelta dell'utente
+const choice = prompt('Scegli pari o dispari');
+const userNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
+
+console.log(choice);
+console.log(userNumber);
