39af3b645b5c98a466cb6920c3d2cd1d795689fb
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-27 16:00:45 +0100

    Feature: Genera Vincitore al click del bottone

--- Pari e Dispari/Bonus/script/myScript.js
+++ Pari e Dispari/Bonus/script/myScript.js
@@ -10,3 +10,29 @@ const userNumber = document.getElementById('user-number');
 // Bottone Play
 const play = document.getElementById('play');
 
+play.addEventListener('click', function() {
+
+  const choiceOutput = choice.value.toLowerCase();
+  const userNumberOutput = parseInt(userNumber.value);
+
+  // Funzione Generazione n. Random computer
+  function getComputerChoice(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  let computerNumber = getComputerChoice(1, 5);
+  
+  const sum = userNumberOutput + computerNumber;
+
+  function determineWinner() {
+    if (sum % 2 === 0 && choiceOutput === 'pari' || sum % 2 === 1 && choiceOutput === 'dispari') {
+      return 'Hai Vinto!'
+    } else {
+      return 'Hai Perso'
+    }
+  }
+  
+})
+
+
+
