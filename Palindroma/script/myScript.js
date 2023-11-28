3a0c7b66ba9b00cbfd9f6ba350697bdfff1bab50
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-28 10:25:57 +0100

    Optimize: variabile reverseWord in function

--- Palindroma/script/myScript.js
+++ Palindroma/script/myScript.js
@@ -1,12 +1,12 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-// Dichiarazione parola (al contrario)
-let reverseWord = '';
-
 // Creazione funzione
 function checkPalindrome(word) {
   
+  // Dichiarazione parola (al contrario)
+  let reverseWord = '';
+
   // Iterazione lettere parola (al contrario)
   for (let i = word.length - 1; i >= 0; i--) {
     reverseWord += word[i];
